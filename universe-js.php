3ba23a5bd874eba38e31e5c99a58e493d9a20730
Joannic Laborde <joannic@sparkart.com>
2020-06-11 11:02:39 -0300

    WordPress plugin

---
+++ universe-js.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name: Universe JS
+ */
+namespace Universe;
+
+function fetch_resource($endpoint) {
+  // TODO store key in plugin setting
+  $url = "https://services.sparkart.net/api/v1/$endpoint" . (strpos($endpoint, '?') === false ? '?key=' : '&key=') . UNIVERSE_KEY;
+
+  // Replace dynamic path segments ({tag}, {topic})
+  $url = preg_replace_callback('/\{(.+?)\}/', function ($match) {return $GLOBALS['solidus_context']['parameters'][$match[1]];}, $url);
+
+  $request = wp_remote_get($url);
+  if (is_wp_error($request)) throw $request;
+  $data = json_decode(wp_remote_retrieve_body($request));
+  if (strcasecmp($data->status, 'error') == 0) throw new \Exception(implode(', ', $data->messages));
+  return $data;
+}
+
+function start_exclusive_content($id = '') {
+  $id = $id ?: rand();
+  ?>
+    <script class="universe">
+      site.universeReady(function(isLoggedIn){
+        isLoggedIn()
+          ? site.render('<?= $id ?>')
+          : site.render('upsell', {redirect: window.location.href.replace(window.location.origin, '')});
+      });
+    </script>
+    <?php get_tmpl('upsell') ?>
+    <template id="tmpl-<?= $id ?>">
+  <?php
+}
+
+function end_exclusive_content() {
+  ?>
+  </template>
+  <?php
+}
+
+function get_tmpl($id) {
+  ?>
+  <template id="tmpl-<?= $id ?>">
+    <?php get_template_part("tmpl/$id") ?>
+  </template>
+  <?php
+}
