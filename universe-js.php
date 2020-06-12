d73478e95b59f9403bebfa19e8b36861fb062695
Joannic Laborde <joannic@sparkart.com>
2020-06-12 11:42:38 -0300

    Get Universe key from context instead of global variable

--- universe-js.php
+++ universe-js.php
@@ -9,12 +9,16 @@
  */
 namespace Universe;
 
-function fetch_resource($endpoint) {
-  // TODO store key in plugin setting
-  $url = "https://services.sparkart.net/api/v1/$endpoint" . (strpos($endpoint, '?') === false ? '?key=' : '&key=') . UNIVERSE_KEY;
+function fetch_resource($endpoint, $universe_api_key = null) {
+  if (!$universe_api_key && $GLOBALS['solidus_context']) $universe_api_key = $GLOBALS['solidus_context']['site']['integrations']['universe_apikey'];
+  if (!$universe_api_key) throw new \Exception('Missing Universe API key');
 
-  // Replace dynamic path segments ({tag}, {topic})
-  $url = preg_replace_callback('/\{(.+?)\}/', function ($match) {return $GLOBALS['solidus_context']['parameters'][$match[1]];}, $url);
+  $url = "https://services.sparkart.net/api/v1/$endpoint" . (strpos($endpoint, '?') === false ? '?key=' : '&key=') . $universe_api_key;
+
+  if ($GLOBALS['solidus_context']) {
+    // Replace dynamic path segments ({tag}, {topic})
+    $url = preg_replace_callback('/\{(.+?)\}/', function ($match) {return $GLOBALS['solidus_context']['parameters'][$match[1]];}, $url);
+  }
 
   $request = wp_remote_get($url);
   if (is_wp_error($request)) throw $request;
